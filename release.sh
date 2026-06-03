4d4591ba5ed631552ac5c112679f2bf619cd359b
Jorge Ivan Armendariz Bejarano <jorgeivanarmendariz1@gmail.com>
2026-06-03 16:40:58 -0600

    Patch Reverb SIGINT for FrankenPHP compatibility

--- release.sh
+++ release.sh
@@ -3,8 +3,8 @@ set -e
 
 php artisan config:clear
 php artisan cache:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 php artisan migrate --force
 php artisan storage:link
+
+# Patch Reverb to work without pcntl
+sed -i 's/return \[SIGINT, SIGTERM, SIGTSTP\];/return [];/' vendor/laravel/reverb/src/Servers/Reverb/Console/Commands/StartServer.php
