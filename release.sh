8b094bbcf592cade985593354d988fa99b0289b2
Jorge Ivan Armendariz Bejarano <jorgeivanarmendariz1@gmail.com>
2026-06-03 15:56:04 -0600

    Move all cache commands to release script

--- release.sh
+++ release.sh
@@ -3,5 +3,8 @@ set -e
 
 php artisan config:clear
 php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 php artisan migrate --force
 php artisan storage:link
