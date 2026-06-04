ef55a36c6eaa72ba5ce9ae5d44ae31aa1fd8b071
Jorge Ivan Armendariz Bejarano <jorgeivanarmendariz1@gmail.com>
2026-06-03 18:13:19 -0600

    Regenerate config cache in release to pick up Cloudinary vars

--- release.sh
+++ release.sh
@@ -3,10 +3,12 @@ set -e
 
 php artisan config:clear
 php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
+php artisan storage:link
 
 # Apply Reverb patch for FrankenPHP (no pcntl)
 cp vendor-patches/laravel/reverb/src/Servers/Reverb/Console/Commands/StartServer.php \
    vendor/laravel/reverb/src/Servers/Reverb/Console/Commands/StartServer.php
-
-php artisan migrate --force
-php artisan storage:link
